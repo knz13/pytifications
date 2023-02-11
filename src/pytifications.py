5acb2c2f2090c255c93044711b7255a7718b1f41
Otavio Maya <82895172+knz13@users.noreply.github.com>
2023-02-11 19:38:07 -0300

    fixing another issue with message editing

--- src/pytifications.py
+++ src/pytifications.py
@@ -71,7 +71,7 @@ class PytificationsMessageWithPhoto:
         for row in buttons:
             rowButtons = []
             for column in row:
-                Pytifications._registered_callbacks[column.callback.__name__] = {"function":column.callback,"args":self}
+                Pytifications._registered_callbacks[column.callback.__name__] = {"function":column.callback,"args":[self]}
                 rowButtons.append({
                     "callback_name":column.callback.__name__,
                     "text":column.text
@@ -139,7 +139,7 @@ class PytificationsMessage:
         for row in buttons:
             rowButtons = []
             for column in row:
-                Pytifications._registered_callbacks[column.callback.__name__] =  {"function":column.callback,"args":self}
+                Pytifications._registered_callbacks[column.callback.__name__] =  {"function":column.callback,"args":[self]}
                 rowButtons.append({
                     "callback_name":column.callback.__name__,
                     "text":column.text
